cc8696f7bdcc47b44b200aefd4b00b50effd0f92
Tudor Zavaliche <t.zavaliche@levi9.com>
2017-03-23 14:12:11 +0200

    Lab4 - Console app

---
+++ Laboratoare/Lab4/ConsoleApp1/ConsoleApp1/ReadWriteQueue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+using System.Threading;
+
+public class ReadWriteQueue
+{
+    public static void SendMessage(string msg)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(msg);
+
+            var properties = channel.CreateBasicProperties();
+            properties.SetPersistent(true);
+
+            channel.BasicPublish(exchange: "", routingKey: "task_queue", basicProperties: properties, body: body);
+            Console.WriteLine(" [x] Sent {0}", msg);
+        }
+
+        Console.WriteLine(" Press [enter] to continue.");
+        Console.ReadLine();
+    }
+
+    public static void ReadMessage()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+
+                int dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 1000);
+
+                Console.WriteLine(" [x] Done");
+
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+            channel.BasicConsume(queue: "task_queue", noAck: false, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
