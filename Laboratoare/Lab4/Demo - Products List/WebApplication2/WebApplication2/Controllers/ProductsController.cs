bf1028095a3c631025dd1a67df52c920f2282384
Claudiu Epure <c.epure@levi9.com>
2017-03-27 10:07:47 +0300

    Add Products - List Demo Project.

---
+++ Laboratoare/Lab4/Demo - Products List/WebApplication2/WebApplication2/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using Market.Infrastructure;
+using Market.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Market.Controllers
+{
+    [Route("api/products")]
+    public class ProductsController : Controller
+    {
+        private Repository repo;
+
+        public ProductsController(Repository repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public IList<Product> GetProducts()
+        {
+            return repo.Products;
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProduct(int id)
+        {
+            return repo.Products.FirstOrDefault(p => p.Id == id);
+        }
+
+        [HttpPost]
+        public void AddProduct([FromBody] Product product)
+        {
+            product.Id = GetNextId();
+
+            repo.Products.Add(product);
+        }
+
+        [HttpPut("{id}")]
+        public void UpdateProduct(int id, [FromBody] Product product)
+        {
+            var productFromRepo = this.repo.Products.FirstOrDefault(p => p.Id == id);
+
+            productFromRepo.Name = product.Name;
+        }
+
+        private int GetNextId()
+        {
+            return new Random().Next(100000);
+        }
+    }
+}
---
+++ Laboratoare/Lab4/Demo - Products List/WebApplication2/WebApplication2/Infrastructure/Repository.cs
@@ -0,0 +1,31 @@
+﻿using Market.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Market.Infrastructure
+{
+    public class Repository
+    {
+        public IList<Product> Products { get; }
+
+        public Repository()
+        {
+            Products = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "beer"
+                },
+
+                new Product
+                {
+                    Id = 2,
+                    Name = "water"
+                }
+            };
+        }
+    }
+}
